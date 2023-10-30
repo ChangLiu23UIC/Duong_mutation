125a921f23f954fc89d8692b948efd1dba53e2ec
Chang Liu <cliu239@uic.edu>
2023-10-30 17:29:34 -0500

    Use the previous written code to find the protein with brackets

--- main.py
+++ main.py
@@ -14,9 +14,37 @@ with open(fasta_name) as fin:
 
 # Read the xlsx file of mutaed segments, only keep the useful columns
 # Also drop the rows where there is no mutation sequence
-mutated = (pd.read_excel("Neoantigen and Damps_zz.xlsx", usecols=[1, 2, 3]).dropna(subset="Neopeptide sequence")
+mutated = (pd.read_excel("Neoantigen and Damps_zz.xlsx", usecols=[0, 1, 2, 3]).dropna(subset="Neopeptide sequence")
            .fillna("No_data"))
-
+# Modify the data
 for index, row in mutated.iterrows():
     if row["Gene origin"] != "No_data":
         row["ID"] = row["Gene origin"]
+mutated = mutated[mutated["Cell line"] != "DAMP"].iloc[:, 1:3]
+
+for index, mutation in mutated.iterrows():
+        if "[" in mutation["Neopeptide sequence"]:
+            original = search_seq(mutation["Neopeptide sequence"])[0]
+            modified = search_seq(mutation["Neopeptide sequence"])[1]
+            # Locate the sequence with gene name, some don't have gene name matched
+            try:
+                seq_index = gene_list.index(mutation["ID"])
+                seq_string = protein_list[seq_index]
+                # replace the original string with the modified string
+                final_sequence = seq_string.replace(original, modified)
+                name = split_list[seq_index].split("\n")[0]
+                # Get the result in fasta format
+                fasta_protein_write = (
+                    (">" + name.split("|")[0] + "|PPP{}|" + name.split("|")[2] + "\n" + final_sequence)
+                    .format(str(str(index).zfill(3)) + "_" + str(1)))
+            except ValueError:
+                n = 0
+                for protein in protein_list:
+                    if protein.find(original) != -1:
+                        n += 1
+                        final_sequence = protein.replace(original, modified)
+                        name = split_list[protein_list.index(protein)].split("\n")[0]
+                        fasta_protein_write = (
+                            (">" + name.split("|")[0] + "|PPP{}|" + name.split("|")[2] + "\n" + final_sequence)
+                            .format(str(str(index).zfill(3)) + "_" + str(n)))
+        # The segment do not have the bracket, but have gene origin name
